da21833761f597dfcf231d879c95ea5ff7c1fb30
Chris <chris@cheney.dev>
2026-01-19 02:41:23 -0600

    chore(agents): remove factor zoo tool definitions
    Remove all factor zoo agent tools including:
    - checkFactorDecay
    - computeMegaAlpha
    - getActiveFactors
    - getCurrentWeights
    - getFactorContext
    - getFactorZooStats
    - runDecayMonitor
    - updateDailyWeights

--- packages/agents/src/tools/definitions/index.ts
+++ packages/agents/src/tools/definitions/index.ts
@@ -64,63 +64,6 @@ export {
 	ExtractNewsContextOutputSchema,
 	extractNewsContextTool,
 } from "./externalContextTools.js";
-// Factor Zoo tools (require FactorZooRepository dependency injection)
-export {
-	type CheckFactorDecayInput,
-	CheckFactorDecayInputSchema,
-	type CheckFactorDecayOutput,
-	CheckFactorDecayOutputSchema,
-	type ComputeMegaAlphaForSymbolsInput,
-	ComputeMegaAlphaForSymbolsInputSchema,
-	type ComputeMegaAlphaForSymbolsOutput,
-	ComputeMegaAlphaForSymbolsOutputSchema,
-	type ComputeMegaAlphaInput,
-	ComputeMegaAlphaInputSchema,
-	type ComputeMegaAlphaOutput,
-	ComputeMegaAlphaOutputSchema,
-	// Check factor decay tool
-	createCheckFactorDecayTool,
-	// Compute Mega-Alpha for symbols tool
-	createComputeMegaAlphaForSymbolsTool,
-	// Compute Mega-Alpha tool
-	createComputeMegaAlphaTool,
-	// Get active factors tool
-	createGetActiveFactorsTool,
-	// Get current weights tool
-	createGetCurrentWeightsTool,
-	// Get factor context tool
-	createGetFactorContextTool,
-	// Get Factor Zoo stats tool
-	createGetFactorZooStatsTool,
-	// Run decay monitor tool
-	createRunDecayMonitorTool,
-	// Update daily weights tool
-	createUpdateDailyWeightsTool,
-	type GetActiveFactorsInput,
-	GetActiveFactorsInputSchema,
-	type GetActiveFactorsOutput,
-	GetActiveFactorsOutputSchema,
-	type GetCurrentWeightsInput,
-	GetCurrentWeightsInputSchema,
-	type GetCurrentWeightsOutput,
-	GetCurrentWeightsOutputSchema,
-	type GetFactorContextInput,
-	GetFactorContextInputSchema,
-	type GetFactorContextOutput,
-	GetFactorContextOutputSchema,
-	type GetFactorZooStatsInput,
-	GetFactorZooStatsInputSchema,
-	type GetFactorZooStatsOutput,
-	GetFactorZooStatsOutputSchema,
-	type RunDecayMonitorInput,
-	RunDecayMonitorInputSchema,
-	type RunDecayMonitorOutput,
-	RunDecayMonitorOutputSchema,
-	type UpdateDailyWeightsInput,
-	UpdateDailyWeightsInputSchema,
-	type UpdateDailyWeightsOutput,
-	UpdateDailyWeightsOutputSchema,
-} from "./factorZoo.js";
 // Filing search tools
 export {
 	FilingChunkSummarySchema,
